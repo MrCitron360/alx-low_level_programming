5c430135de811f912968fc53a507108875629ff5
MrCitron360 <Austinebri@gmail.com>
2022-11-01 05:36:43 -0700

    scripts 0 to 8

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strspn - get length of a prefix substring
+ * @s: string to compare against substring
+ * @accept: accepted characters to count
+ *
+ * Return: bytes with characters
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j, match, bytes;
+
+	for (i = 0; s[i]; i++)
+	{
+		match = 0;
+		for (j = 0; accept[j]; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				match = 1;
+				bytes++;
+			}
+		}
+		if (match == 0)
+			return (bytes);
+	}
+	return (0);
+}
